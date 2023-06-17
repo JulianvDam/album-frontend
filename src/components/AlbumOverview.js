7f5f53c3217606042d0b98254adf278513bb8ecc
JulianvDam <1038219@hr.nl>
2023-06-17 16:38:07 +0200

    Lesson 6: Workflow S3 Bucket

--- src/components/AlbumOverview.js
+++ src/components/AlbumOverview.js
@@ -3,28 +3,31 @@ import { Grid } from '@material-ui/core';
 import AlbumCard from './AlbumCard';
 import useAlbums from "../hooks/useAlbums";
 
+export const AlbumOverview = ({ albums }) => {
+  if (!albums || !albums.length) {
+    return <div>Loading albums...</div>;
+  }
 
-function AlbumOverview() {
-    const albums = useAlbums();
-  
-    if (!albums.length) {
-      return <div>Loading albums...</div>;
-    }
-  
-    return (
-      <div>
-        {albums.map((album) => (
+  return (
+    <Grid container spacing={2}>
+      {albums.map((album) => (
+        <Grid item xs={12} sm={6} md={4} key={album.id}>
           <AlbumCard
-            key={album.id}
             id={album.id}
             name={album.name}
             artist={album.artist}
             imageUrl={album.imageUrl}
           />
-        ))}
-      </div>
-    );
-  }
-  
-  export default AlbumOverview;
-  
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export const AlbumOverviewWrapper = () => {
+  const albums = useAlbums();
+
+  return <AlbumOverview albums={albums} />;
+};
+
+export default AlbumOverview;
