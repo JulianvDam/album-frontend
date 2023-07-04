b56851c6140ba7b2ef4c8892e68c5bac9e4188cb
JulianvDam <1038219@hr.nl>
2023-07-04 18:52:53 +0200

    Lesson 6: Bugfixes

--- src/components/CreateAlbum.js
+++ src/components/CreateAlbum.js
@@ -5,21 +5,44 @@ import AlbumForm from './AlbumForm';
 const CreateAlbum = () => {
   const navigate = useNavigate();
 
+  // const handleSubmit = (newAlbum) => {
+  //   console.log(newAlbum);
+  //   const request = fetch('/api/albums', {
+  //     method: 'POST',
+  //     body: JSON.stringify(newAlbum),
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
+
+  //   request.then((response) => {
+  //     if (!response.ok) {
+  //       console.error(response.statusText);
+  //     } else {
+  //       navigate('/');
+  //     }
+  //   });
+  // };
+
+  // return (
+  //   <div>
+  //     <Link to="/">Back to Overview</Link>
+  //     <AlbumForm onSubmit={handleSubmit} />
+  //   </div>
+  // );
   const handleSubmit = (newAlbum) => {
     console.log(newAlbum);
-    const request = fetch('/api/albums', {
-      method: 'POST',
-      body: JSON.stringify(newAlbum),
-      headers: { 'Content-Type': 'application/json' },
-    });
 
-    request.then((response) => {
-      if (!response.ok) {
-        console.error(response.statusText);
-      } else {
+    // Replace the connection string and the following code with your actual database integration logic
+    const connectionString = "Server=cnsd-db-716882742183.cuwkdwslmzed.us-east-1.rds.amazonaws.com;Port=5432;Database=albumdatabase;User Id=julian;Password=heelsterkpass;";
+    const fakeDatabase = connectToDatabase(connectionString);
+
+    // Example code using a fake database connection
+    fakeDatabase.insertAlbum(newAlbum)
+      .then(() => {
         navigate('/');
-      }
-    });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
@@ -31,3 +54,17 @@ const CreateAlbum = () => {
 };
 
 export default CreateAlbum;
+
+function connectToDatabase(connectionString) {
+  return {
+    insertAlbum: (album) => {
+      return new Promise((resolve, reject) => {
+        // Simulate database insert
+        setTimeout(() => {
+          console.log('Inserting album into the database:', album);
+          resolve();
+        }, 1000);
+      });
+    }
+  };
+}
