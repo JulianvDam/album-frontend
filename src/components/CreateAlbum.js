7ca30f196c61a2663172fe648b58229333cbc886
JulianvDam <1038219@hr.nl>
2023-07-04 17:55:34 +0200

    Lesson 6: Bugfixes

--- src/components/CreateAlbum.js
+++ src/components/CreateAlbum.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import AlbumForm from './AlbumForm';
 
 const CreateAlbum = () => {
